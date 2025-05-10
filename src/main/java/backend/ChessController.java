6bf95813b4b5213b43a06102092b0ba493a97899
C2H6Ethan <ejbaum@live.com>
2025-05-10 14:21:09 +0200

    added minimal spring boot setup

---
+++ src/main/java/backend/ChessController.java
@@ -0,0 +1,20 @@
+package backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+class ChessController {
+    private final ChessService chessService;
+
+    public ChessController(ChessService chessService) {
+        this.chessService = chessService;
+    }
+
+    @GetMapping("/board")
+    public String getBoard() {
+        return chessService.getBoard();
+    }
+}
---
+++ src/main/java/backend/ChessService.java
@@ -0,0 +1,18 @@
+package backend;
+
+import logic.Board;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChessService {
+    private final Board game;
+
+    public ChessService() {
+        game = new Board();
+        game.setupPieces();
+    }
+
+    public String getBoard() {
+        return "test board";
+    }
+}
