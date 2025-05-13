297016adb5b14987dd21370908169a88743a0587
Ethan Baumgartner <ejbaum@live.com>
2025-05-13 18:11:13 +0200

    forgot to push GameState.java

--- src/main/java/logic/GameState.java
+++ src/main/java/logic/GameState.java
@@ -4,26 +4,16 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class GameState {
-    private final Square[][] squares;
+    private final String squares;
     private final String player;
     private final CastlingRights castlingRights;
     private final Square enPassantTarget;
 
-    public GameState(Square[][] squares, String player, CastlingRights castlingRights, Square enPassantTarget) {
-        this.squares = deepCopySquares(squares);
+    public GameState(String fen, String player, CastlingRights castlingRights, Square enPassantTarget) {
+        this.squares = fen.split(" ")[0]; //gets only the squares information from the fen string
         this.player = player;
         this.castlingRights = castlingRights.copy();
         this.enPassantTarget = enPassantTarget != null ? enPassantTarget.copy() : null;
-    }
-
-    private Square[][] deepCopySquares(Square[][] original) {
-        Square[][] copy = new Square[8][8];
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                copy[row][col] = original[row][col].copy();
-            }
-        }
-        return copy;
     }
 
     public Square getEnPassantTarget() {
@@ -45,7 +35,7 @@ public class GameState {
         GameState gameState = (GameState) o;
 
         // Third condition
-        boolean squaresEqual = Objects.deepEquals(squares, gameState.squares);
+        boolean squaresEqual = Objects.equals(squares, gameState.squares);
         if (!squaresEqual) {
             return false;
         }
@@ -74,7 +64,7 @@ public class GameState {
 
     @Override
     public int hashCode() {
-        return Objects.hash(Arrays.deepHashCode(squares), player, castlingRights, enPassantTarget);
+        return Objects.hash(squares, player, castlingRights, enPassantTarget);
     }
 
 }
